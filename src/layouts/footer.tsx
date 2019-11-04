3f31d582b864dcf1e4167692b87fc736fe4c2674
OhYee <oyohyee@oyohyee.com>
2019-11-04 23:14:24 +0800

    misc: footer part(#master)

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,10 +1,70 @@
 import React from 'react';
-class FooterPart extends React.Component<{}, {}> {
+import { Divider, Row, Col, Table, Icon } from 'antd';
+
+import Container from '@/components/container';
+import Link from 'umi/link';
+
+import styles from './footer.less';
+import { requestCallback } from '@/utils/request';
+
+import { ColumnProps } from 'antd/es/table';
+
+type FooterState = {
+  site: Blotter.Site;
+};
+class FooterPart extends React.Component<{}, FooterState> {
+  columns: ColumnProps<Blotter.Site>[] = [
+    {
+      title: 'k',
+      key: 'left',
+      dataIndex: 'left',
+    },
+    {
+      title: 'v',
+      key: 'right',
+      dataIndex: 'right',
+    },
+  ];
   constructor(props: any) {
     super(props);
+    this.state = { site: { view: 0, beian: '', friends: [] } };
   }
+
+  componentDidMount() {
+    requestCallback('get', '/api/site', {}, (data: any) => this.setState({ site: data }));
+  }
+
+  makeDateSource = () => {
+    var keys: ('view' | 'beian')[] = ['view', 'beian'];
+    var dataSource = [];
+    for (var i = 0; i < keys.length; i++) {
+      dataSource.push({
+        key: i,
+        left: keys[i],
+        right: this.state.site[keys[i]],
+      });
+    }
+    console.log(dataSource);
+    return dataSource;
+  }
+
   render() {
-    return <p>Footer</p>;
+    return (
+      <Container>
+        <div className={styles.footer}>
+          <p>© 2017 – {new Date().getFullYear()}</p>
+          <p>
+            <Icon type="eye" style={{ fontSize: '0.75em' }} /> 全站访问量 {this.state.site.view}
+          </p>
+          <p>
+            <Link to="http://beian.miit.gov.cn/">{this.state.site.beian}</Link>
+          </p>
+          <p>
+            Powered by <Link to="github.com/OhYee/blotter">Blotter</Link> (Go + React)
+          </p>
+        </div>
+      </Container>
+    );
   }
 }
 
