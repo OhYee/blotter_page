a843eb849043f31e404a70958b4a63d6bd476325
OhYee <oyohyee@oyohyee.com>
2020-01-30 20:46:30 +0800

    misc: using the real backend not mock(#master)

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
-export const requestAsync = async (
-  method: 'post' | 'get',
-  url: string,
-  data: any,
-) => {
+const domain = 'http://127.0.0.1:50000';
+
+function parseURL(url: string): string {
+  return domain + url;
+  //   return url == '/api/friends' || url == '/api/menu' || url == '/api/post' || url == '/api/posts' || url == '/api/comments'
+  //     ? domain + url
+  //     : url;
+}
+
+export const requestAsync = async (method: 'post' | 'get', url: string, data: any) => {
   var r = await axios({
     method: method,
-    url: url,
+    url: parseURL(url),
     params: data,
   });
   return r.data;
@@ -21,10 +26,27 @@ export const requestCallback = (
 ) => {
   axios({
     method: method,
-    url: url,
+    url: parseURL(url),
     params: data,
   })
     .then(data => data.data)
     .then(data => callback(data))
     .catch(err => console.log(err));
+};
+
+export const request = async (
+  method: 'post' | 'get',
+  url: string,
+  data: any,
+  callback?: (data: any) => void,
+) => {
+  var r = await axios({
+    method: method,
+    url: parseURL(url),
+    params: data,
+  });
+  if (callback) {
+    callback(r.data);
+  }
+  return r.data;
 };
