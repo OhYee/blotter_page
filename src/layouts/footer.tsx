ff3761e7fc447ab7d67fda351ad02b045a316f63
OhYee <oyohyee@oyohyee.com>
2020-01-30 20:48:42 +0800

    misc: query footer data from backend(#master)

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -10,7 +10,8 @@ import { requestCallback } from '@/utils/request';
 import { ColumnProps } from 'antd/es/table';
 
 type FooterState = {
-  site: Blotter.Site;
+  view: number;
+  beian: string;
 };
 class FooterPart extends React.Component<{}, FooterState> {
   columns: ColumnProps<Blotter.Site>[] = [
@@ -27,26 +28,32 @@ class FooterPart extends React.Component<{}, FooterState> {
   ];
   constructor(props: any) {
     super(props);
-    this.state = { site: { view: 0, beian: '', friends: [] } };
+    this.state = { view: 0, beian: '' };
   }
 
   componentDidMount() {
-    requestCallback('get', '/api/site', {}, (data: any) => this.setState({ site: data }));
+    requestCallback(
+      'get',
+      '/api/variables',
+      { keys: 'view,beian' },
+      (data: { beian: string; view: number }) =>
+        this.setState({ beian: data.beian, view: data.view }),
+    );
   }
 
-  makeDateSource = () => {
-    var keys: ('view' | 'beian')[] = ['view', 'beian'];
-    var dataSource = [];
-    for (var i = 0; i < keys.length; i++) {
-      dataSource.push({
-        key: i,
-        left: keys[i],
-        right: this.state.site[keys[i]],
-      });
-    }
-    console.log(dataSource);
-    return dataSource;
-  }
+//   makeDateSource = () => {
+//     var keys: ('view' | 'beian')[] = ['view', 'beian'];
+//     var dataSource = [];
+//     for (var i = 0; i < keys.length; i++) {
+//       dataSource.push({
+//         key: i,
+//         left: keys[i],
+//         right: this.state.site[keys[i]],
+//       });
+//     }
+//     console.log(dataSource);
+//     return dataSource;
+//   };
 
   render() {
     return (
@@ -54,10 +61,10 @@ class FooterPart extends React.Component<{}, FooterState> {
         <div className={styles.footer}>
           <p>© 2017 – {new Date().getFullYear()}</p>
           <p>
-            <Icon type="eye" style={{ fontSize: '0.75em' }} /> 全站访问量 {this.state.site.view}
+            <Icon type="eye" style={{ fontSize: '0.75em' }} /> 全站访问量 {this.state.view}
           </p>
           <p>
-            <Link to="http://beian.miit.gov.cn/">{this.state.site.beian}</Link>
+            <Link to="http://beian.miit.gov.cn/">{this.state.beian}</Link>
           </p>
           <p>
             Powered by <Link to="github.com/OhYee/blotter">Blotter</Link> (Go + React)
