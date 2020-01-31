6f7725a495bc4da57f9c6dce75986f664eab0068
OhYee <oyohyee@oyohyee.com>
2020-01-31 21:29:44 +0800

    fix: not all location has query params(#master)

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,13 +1,14 @@
-export type LocationWithQuery = {
-  query: { [key: string]: string };
-} & Location;
+export const parseNumberParams = (key: string, location: Location, defaultValue: number = 1) => {
+  var query = location.search
+    .slice(1)
+    .split('&')
+    .reduce((obj: { [key: string]: string }, data: string, i: number) => {
+      var t = data.split('=');
+      obj[t[0]] = t[1];
+      return obj;
+    }, {});
 
-export const parseNumberParams = (
-  key: string,
-  location: LocationWithQuery,
-  defaultValue: number = 1,
-) => {
-  var data = parseInt(location.query[key]);
+  var data = parseInt(query[key]);
   if (data < 1 || isNaN(data)) {
     data = defaultValue;
   }
