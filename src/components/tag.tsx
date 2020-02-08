e8cef91efe42c2944dd3435f9c004d6c6b0df25f
OhYee <oyohyee@oyohyee.com>
2020-02-09 02:33:55 +0800

    misc: tag could close(#master)

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -4,18 +4,31 @@ import Link from 'umi/link';
 
 import styles from './tag.less';
 
-type TagProps = {
-  tag: Blotter.Tag;
-};
+interface TagProps {
+  tag?: Blotter.Tag;
+  closable?: boolean;
+  onClose?: (tag: Blotter.Tag) => void;
+}
 
 class TagPart extends React.Component<TagProps, {}> {
+  static defaultProps: TagProps = {
+    closable: false,
+    onClose: () => {},
+  };
   constructor(props: TagProps) {
     super(props);
   }
 
   render() {
-    return (
-      <Tag className={styles.tag} color={this.props.tag.color}>
+    return typeof this.props.tag === 'undefined' ? null : (
+      <Tag
+        className={styles.tag}
+        color={this.props.tag.color}
+        closable={this.props.closable}
+        onClose={() => {
+          this.props.onClose!(this.props.tag!);
+        }}
+      >
         <Link to={`/tag/${this.props.tag.short}`}>
           {this.props.tag.icon ? (
             <Avatar size={15} shape="circle" src={this.props.tag.icon} />
