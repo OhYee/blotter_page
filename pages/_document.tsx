903b8aa0f150140c4ae5684b7aa3ff3cf97eaf7d
OhYee <oyohyee@oyohyee.com>
2020-02-15 00:36:29 +0800

    misc: head meat(#master)

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Blotter</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="theme-color" content="#1565c0" />
+          <meta name="msapplication-TileColor" content="#1565c0" />
+          <meta name="msapplication-TileImage" content="icon.png" />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
+            integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
