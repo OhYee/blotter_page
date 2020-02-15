ea35f8dcc69b00991170660fc162ac813986ff94
OhYee <oyohyee@oyohyee.com>
2020-02-16 01:25:16 +0800

    misc: add crossOrigin to css(#master)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
             integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
+            crossOrigin="anonymous"
           ></link>
         </Head>
         <body>
