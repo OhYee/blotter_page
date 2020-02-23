1ef7de6c0a6f819e4a991d77d2b7df2749ad8c8f
OhYee <oyohyee@oyohyee.com>
2020-02-24 00:53:39 +0800

    misc: add baidu site statistics(#master)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,20 +21,34 @@ export default class MyDocument extends Document {
             integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
             crossOrigin="anonymous"
           ></link>
+          {/* 百度自动推送 */}
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(){
-            var bp = document.createElement('script');
-            var curProtocol = window.location.protocol.split(':')[0];
-            if (curProtocol === 'https') {
-                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-            }
-            else {
-                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-            }
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(bp, s);
-        })();`,
+              __html: `// 百度自动推送
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();`,
+            }}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9f114bc8c6227268e6d0e44abd656932";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();`,
             }}
           ></script>
         </Head>
