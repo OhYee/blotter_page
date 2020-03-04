dedbcd2b1bc42563553f218c65ec7f89b57e070b
OhYee <oyohyee@oyohyee.com>
2020-03-04 21:07:41 +0800

    fix: redirect when url is post/xxx/xxx/(#master)

--- utils/redirect.ts
+++ utils/redirect.ts
@@ -1,14 +1,16 @@
+var hasPrefix = (s: string, prefix: string) => {
+  const l = prefix.length;
+  return s.length >= l && s.slice(0, l) === prefix;
+};
+
+var hasSuffix = (s: string, suffix: string) => {
+  const l = s.length;
+  const l2 = suffix.length;
+  return l >= l2 && s.slice(l - l2) === suffix;
+};
+
 export const guessPath = (pathname: string) => {
   var path = '';
-  var hasPrefix = (s: string, prefix: string) => {
-    const l = prefix.length;
-    return s.length >= l && s.slice(0, l) === prefix;
-  };
-  var hasSuffix = (s: string, suffix: string) => {
-    const l = s.length;
-    const l2 = suffix.length;
-    return l >= l2 && s.slice(l - l2) === suffix;
-  };
 
   if (hasPrefix(pathname, '/post/')) {
     var url = pathname
@@ -16,6 +18,7 @@ export const guessPath = (pathname: string) => {
       .toLocaleLowerCase()
       .replace('/', '_');
     url = hasSuffix(url, '.html') ? url.slice(0, url.length - 5) : url;
+    url = hasSuffix(url, '_') ? url.slice(0, url.length - 1) : url;
     path = `/post/${url}`;
   }
   if (hasPrefix(pathname, '/tag/')) {
@@ -24,6 +27,7 @@ export const guessPath = (pathname: string) => {
       .toLocaleLowerCase()
       .replace('/', '_');
     url = hasSuffix(url, '.html') ? url.slice(0, url.length - 5) : url;
+    url = hasSuffix(url, '_') ? url.slice(0, url.length - 1) : url;
     path = `/tag/${url}`;
   }
   if (path == pathname) {
