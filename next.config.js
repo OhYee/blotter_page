b8689ff42d395cd748706cc1a8c699c130ff0382
OhYee <oyohyee@oyohyee.com>
2020-04-29 00:58:30 +0800

    style: code style(#master)

--- next.config.js
+++ next.config.js
@@ -40,10 +40,10 @@ withAntd = (nextConfig = {}) => {
     webpack(config, options) {
       if (config.externals) {
         const includes = [/antd/];
-        config.externals = config.externals.map(external => {
+        config.externals = config.externals.map((external) => {
           if (typeof external !== 'function') return external;
           return (ctx, req, cb) => {
-            return includes.find(include =>
+            return includes.find((include) =>
               req.startsWith('.') ? include.test(path.resolve(ctx, req)) : include.test(req),
             )
               ? cb()
