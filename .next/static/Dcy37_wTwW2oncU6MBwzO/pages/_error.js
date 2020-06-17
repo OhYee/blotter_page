9a3cbea771ae4337cad171f0cf58a1a64299f46a
OhYee <oyohyee@oyohyee.com>
2020-06-17 14:11:12 +0000

    Update gh-pages to output generated at 8fe35b3

--- .next/static/Dcy37_wTwW2oncU6MBwzO/pages/_error.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{"4JYB":function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return o("D4/9")}])}},[["4JYB",1,2,0,3,5,4,6,14,13,15,22,33,38,39]]]);
--- next.config.js
+++ next.config.js
@@ -10,113 +10,16 @@ const withPlugins = require('next-compose-plugins');
 const path = require('path');
 const withTM = require('next-transpile-modules');
 const cssLoaderGetLocalIdent = require('css-loader/lib/getLocalIdent.js');
-// const generateTheme = require('next-dynamic-antd-theme/plugin');
+const generateTheme = require('next-dynamic-antd-theme/plugin');
 
-// =================================
-const fs = require('fs');
-const { generateTheme, getLessVars } = require('antd-theme-generator');
 
-class Plugin {
-  constructor(generator) {
-    this.generator = generator;
-  }
-
-  apply(compiler) {
-    compiler.hooks.emit.tapAsync('NextDynamicAntDesignPlugin', async (compilation, callback) => {
-      this.generator();
-      callback();
-    });
-  }
-}
-
-function generateTheme2(themeOptions) {
-  var {
-    antDir = path.join('node_modules/antd'),
-    antdStylesDir = path.join(antDir, 'lib'),
-    stylesDir,
-    varFile,
-    outputFilePath,
-    cssModules,
-    themeVariables,
-    customColorRegexArray = [],
-  } = themeOptions;
-
-  // read themes
-  var themes = {};
-  const files = fs.readdirSync(path.join(antdStylesDir, 'style/themes'));
-  const reg = /(.*)\.less/;
-  files.map((file) => {
-    const res = reg.exec(file);
-    if (!!res && res[1] !== 'index') {
-      const name = res[1];
-      const filepath = path.join(antdStylesDir, 'style/themes', file);
-      themes[name] = getLessVars(filepath);
-    }
-  });
-  if (!themeVariables) {
-    themeVariables = Object.keys(themes.dark);
-    // themeVariables = Array.from(
-    //   new Set(
-    //     Object.values(themes)
-    //       .map((item) => Object.keys(item))
-    //       .reduce((a, b) => a.concat(b)),
-    //   ),
-    // );
-  }
-  console.log(themeVariables);
-  const generator = async () => {
-    try {
-      const dir = path.dirname(themeOptions.outputFilePath);
-      if (!(await fs.existsSync(dir))) {
-        await fs.mkdirSync(dir);
-      }
-
-      await generateTheme({
-        antDir,
-        antdStylesDir,
-        stylesDir,
-        varFile,
-        outputFilePath,
-        cssModules,
-        themeVariables,
-        customColorRegexArray,
-      });
-    } catch (e) {
-      console.log('Error', e);
-    }
-  };
-  console.log(themes);
-  return (nextConfig = {}) => {
-    return Object.assign({}, nextConfig, {
-      publicRuntimeConfig: {
-        // Will be available on both server and client
-        ...nextConfig.publicRuntimeConfig,
-        next_dynamic_antd_themes: themes,
-      },
-      lessLoaderOptions: {
-        javascriptEnabled: true,
-      },
-      webpack(config, options) {
-        // config.node = { fs: 'empty' };
-        config.plugins.push(new Plugin(generator));
-
-        return typeof nextConfig.webpack === 'function'
-          ? nextConfig.webpack(config, options)
-          : config;
-      },
-    });
-  };
-}
-// ==============================
-//==========================
 
 const prod = process.env.NODE_ENV === 'production';
 
-const withAntdTheme = generateTheme2({
+const withAntdTheme = generateTheme({
   antDir: path.join(__dirname, './node_modules/antd'),
   stylesDir: path.join(__dirname, './theme'),
   varFile: path.join(__dirname, './theme/vars.less'),
-  mainLessFile: path.join(__dirname, './theme/main.less'),
   outputFilePath: path.join(__dirname, './.next/static/color.less'),
 });
 
