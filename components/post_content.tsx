15ff1e2dace7647fb3980f780e06f7b10860fc50
OhYee <oyohyee@oyohyee.com>
2020-07-03 21:52:12 +0800

    misc: show carousel not headimage(#master)

--- components/post_content.tsx
+++ components/post_content.tsx
@@ -11,6 +11,8 @@ import TagPart from '@/components/tag';
 import { Context } from '@/utils/global';
 
 import styles from '@/pages/post/post.less';
+import Carousel from './carousel';
+import { Space } from './container';
 
 interface PostContentProps {
   post: {
@@ -23,6 +25,7 @@ interface PostContentProps {
     publish_time: string;
     edit_time: string;
     tags: Blotter.Tag[];
+    images: string[];
   };
 }
 
@@ -47,49 +50,53 @@ class PostContent extends Component<PostContentProps> {
       <Skeleton active={true} />
     ) : (
       <article className={styles.post}>
-        <PageHeader className="shadow" title={this.props.post.title}>
-          <div>
-            <div className="right20">
-              <EyeOutlined className="right5" />
-              {this.props.post.view}
-            </div>
-            <div className="right20">
-              <CalendarOutlined className="right5" />
-              {this.props.post.publish_time}
-            </div>
-            {this.props.post.publish_time == this.props.post.edit_time ? null : (
-              <div className="right20">
-                <EditOutlined className="right5" />
-                {this.props.post.edit_time}
-              </div>
-            )}
-            <Context.Consumer>
-              {(context) =>
-                (context.user.permission & 1) == 1 ? (
-                  <Link href={`/admin/post?url=${this.props.post.url}`}>
-                    <Button type="primary" size="small">
-                      编辑
-                    </Button>
-                  </Link>
-                ) : null
-              }
-            </Context.Consumer>
-            <If condition={!!this.props.post.abstract}>
-              <blockquote>{this.props.post.abstract}</blockquote>
-            </If>
+        <Space size={20}>
+          <PageHeader className="shadow" title={this.props.post.title}>
             <div>
-              <TagOutlined className="right20" />
-              {this.props.post.tags.length > 0 ? (
-                this.props.post.tags.map((tag: Blotter.Tag) => (
-                  <TagPart key={tag.short} tag={tag} />
-                ))
-              ) : (
-                <span>无标签</span>
+              <div className="right20">
+                <EyeOutlined className="right5" />
+                {this.props.post.view}
+              </div>
+              <div className="right20">
+                <CalendarOutlined className="right5" />
+                {this.props.post.publish_time}
+              </div>
+              {this.props.post.publish_time == this.props.post.edit_time ? null : (
+                <div className="right20">
+                  <EditOutlined className="right5" />
+                  {this.props.post.edit_time}
+                </div>
               )}
+              <Context.Consumer>
+                {(context) =>
+                  (context.user.permission & 1) == 1 ? (
+                    <Link href={`/admin/post?url=${this.props.post.url}`}>
+                      <Button type="primary" size="small">
+                        编辑
+                      </Button>
+                    </Link>
+                  ) : null
+                }
+              </Context.Consumer>
+              <If condition={!!this.props.post.abstract}>
+                <blockquote>{this.props.post.abstract}</blockquote>
+              </If>
+              <div>
+                <TagOutlined className="right20" />
+                {this.props.post.tags.length > 0 ? (
+                  this.props.post.tags.map((tag: Blotter.Tag) => (
+                    <TagPart key={tag.short} tag={tag} />
+                  ))
+                ) : (
+                  <span>无标签</span>
+                )}
+              </div>
             </div>
-          </div>
-        </PageHeader>
-        <If condition={this.props.post.head_image !== ''}>
+          </PageHeader>
+          <If condition={!!this.props.post.images}>
+            <Carousel images={this.props.post.images} height="500px" maxHeight={'50vh'} autoplay />
+          </If>
+          {/* <If condition={this.props.post.head_image !== ''}>
           <a
             className="headimage image"
             target="_blank"
@@ -98,11 +105,12 @@ class PostContent extends Component<PostContentProps> {
           >
             <img src={this.props.post.head_image} alt={this.props.post.title} />
           </a>
-        </If>
-        <section
-          className="post-content"
-          dangerouslySetInnerHTML={{ __html: this.props.post.content }}
-        />
+        </If> */}
+          <section
+            className="post-content"
+            dangerouslySetInnerHTML={{ __html: this.props.post.content }}
+          />
+        </Space>
       </article>
     );
   }
