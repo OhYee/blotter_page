3486faa4b4f6b8254b4ae3657c2e0fed6fbb09e7
OhYee <oyohyee@oyohyee.com>
2020-07-04 21:01:44 +0800

    misc: rewrite post_content(#master)

--- components/post_content.tsx
+++ components/post_content.tsx
@@ -4,11 +4,16 @@ import Link from 'next/link';
 import { Skeleton, PageHeader, Button } from 'antd';
 import { EyeOutlined, CalendarOutlined, EditOutlined, TagOutlined } from '@ant-design/icons';
 
-import { setImageLightbox } from '@/components/image';
+import moment from 'moment';
+
+import Image, { setImageLightbox } from '@/components/image';
 import If from '@/components/if';
 import TagPart from '@/components/tag';
+import StaticMap from '@/components/static_map';
+import FocusDiv from '@/components/focus';
 
 import { Context } from '@/utils/global';
+import { travels_get_url } from '@/utils/api';
 
 import styles from '@/pages/post/post.less';
 import Carousel from './carousel';
@@ -29,7 +34,15 @@ interface PostContentProps {
   };
 }
 
-class PostContent extends Component<PostContentProps> {
+interface PostContentState {
+  travel?: Blotter.Travel;
+}
+
+class PostContent extends Component<PostContentProps, PostContentState> {
+  constructor(props) {
+    super(props);
+    this.state = { travel: undefined };
+  }
   resetImage() {
     const containers = document.getElementsByClassName('image');
     for (var i = 0; i < containers.length; i++) {
@@ -41,10 +54,64 @@ class PostContent extends Component<PostContentProps> {
   }
   componentDidMount() {
     this.resetImage();
+    if (this.isTravel()) {
+      this.getTravelData();
+    }
   }
   componentDidUpdate() {
     this.resetImage();
   }
+
+  isTravel = () => {
+    return (
+      this.props.post.tags.filter((item) => item.name === '游记' && item.short === 'travels')
+        .length > 0
+    );
+  };
+
+  getTravelData = () => {
+    travels_get_url(this.props.post.url)
+      .then((r) => {
+        if (r.exist) {
+          this.setState({ travel: r.travel });
+        }
+      })
+      .catch(console.error);
+  };
+
+  renderTravel = () => {
+    const travel = this.state.travel;
+    if (!!travel) {
+      const lnglat = `${travel.lng},${travel.lat}`;
+      return (
+        <FocusDiv distance={0.5}>
+          {(inArea) => (
+            <div>
+              <Image
+                clickable
+                height={inArea ? '512px' : '100px'}
+                style={{ transition: 'height .5s' }}
+                maxHeight="50vh"
+                src={`https://restapi.amap.com/v3/staticmap?location=${lnglat}&zoom${travel.zoom}&scale=2&size=1024*1024&markers=mid,,A:${lnglat}&key=5918961af3c0ffafa1a3cb1056556c98`}
+                alt={`${travel.name}地图`}
+                title={`${moment(travel.time, 'X').format('YYYY-MM-DD')} 于 ${travel.name}`}
+              />
+              <p style={{ textAlign: 'center', fontSize: '1.25em' }}>
+                该文章是一篇 <strong>{moment(travel.time, 'X').format('YYYY-MM-DD')}</strong> 于{' '}
+                <strong>{travel.name}</strong> 的游记， 更多游记见
+                <a href="/apps/travels" target="_blank">
+                  游记列表
+                </a>
+              </p>
+            </div>
+          )}
+        </FocusDiv>
+      );
+    } else {
+      return null;
+    }
+  };
+
   render() {
     return this.props.post === undefined ? (
       <Skeleton active={true} />
@@ -93,8 +160,23 @@ class PostContent extends Component<PostContentProps> {
               </div>
             </div>
           </PageHeader>
+
+          {this.renderTravel()}
+
           <If condition={!!this.props.post.images}>
-            <Carousel images={this.props.post.images} height="500px" maxHeight={'50vh'} autoplay />
+            <FocusDiv distance={0.5}>
+              {(inArea) => {
+                return (
+                  <Carousel
+                    images={this.props.post.images}
+                    height={inArea ? '500px' : '100px'}
+                    maxHeight={'50vh'}
+                    autoplay
+                    style={{ transition: 'height .5s' }}
+                  />
+                );
+              }}
+            </FocusDiv>
           </If>
           {/* <If condition={this.props.post.head_image !== ''}>
           <a
