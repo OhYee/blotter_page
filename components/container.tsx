99302c76bba38d1f9f2528385a4e648ccfa42ec0
OhYee <oyohyee@oyohyee.com>
2020-08-12 01:41:23 +0800

    feat: Flex component for quick layout(#master)

--- components/container.tsx
+++ components/container.tsx
@@ -1,7 +1,7 @@
 import React, { ComponentProps } from 'react';
 import { Row, Col, Space as S } from 'antd';
 import { Gutter } from 'antd/lib/grid/row';
-import '@/components/layout.less';
+import { ObjectFilter } from '@/utils/object';
 
 interface ContainerProps extends ComponentProps<'base'> {
   xs?: number;
@@ -108,3 +108,130 @@ const FlexCenter: React.FC = (props) => {
 
 export default Container;
 export { Space, TextCenter, FlexCenter };
+
+export declare type SizeProp = number | 'none' | 'small' | 'middle' | 'large';
+
+function getSize(size: SizeProp): number {
+  return size === 'large'
+    ? 20
+    : size === 'middle'
+    ? 10
+    : size === 'small'
+    ? 5
+    : size === 'none'
+    ? 0
+    : size;
+}
+
+export const Flex: React.FC<{
+  direction?: 'LR' | 'TB' | 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  wrap?: true | false | 'nowrap' | 'wrap' | 'wrap-reverse';
+  mainAxis?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around';
+  subAxis?: 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch';
+  mainSize?: SizeProp;
+  subSize?: SizeProp;
+  subAxisSizeEnable?: boolean;
+  style?: React.CSSProperties;
+  itemStyle?: React.CSSProperties;
+  className?: string;
+}> = (props) => {
+  const {
+    direction = 'LR',
+    wrap = true,
+    mainSize = 'middle',
+    subSize = 0,
+    mainAxis = 'space-between',
+    subAxis = 'center',
+    itemStyle = {},
+    children,
+    style,
+    className,
+  } = props;
+  const list = Array.isArray(children) ? children : [children];
+  const dir = direction === 'LR' ? 'row' : direction === 'TB' ? 'column' : direction;
+  const mSize = getSize(mainSize);
+  const sSize = getSize(subSize);
+
+  // 容器样式
+  const containerStyles: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: dir,
+    flexWrap: wrap === true ? 'wrap' : wrap === false ? 'nowrap' : wrap,
+    justifyContent: mainAxis,
+    alignItems: subAxis,
+  };
+
+  // 元素样式 - 计算每个元素之间的边距
+  var defaultStyle = {};
+  var specialStyle = {};
+  var after = 'marginRight';
+  var before = 'marginLeft';
+  var subAxis1 = 'marginTop';
+  var subAxis2 = 'marginBottom';
+  const specialPos =
+    mainAxis === 'space-around'
+      ? dir === 'row' || dir === 'column'
+        ? 0
+        : list.length - 1
+      : dir === 'row' || dir === 'column'
+      ? list.length - 1
+      : 0;
+  switch (dir) {
+    case 'row': {
+      after = 'marginRight';
+      before = 'marginLeft';
+      subAxis1 = 'marginTop';
+      subAxis2 = 'marginBottom';
+      break;
+    }
+    case 'column': {
+      after = 'marginBottom';
+      before = 'marginTop';
+      subAxis1 = 'marginLeft';
+      subAxis2 = 'marginRight';
+      break;
+    }
+    case 'row-reverse': {
+      after = 'marginLeft';
+      before = 'marginRight';
+      subAxis1 = 'marginTop';
+      subAxis2 = 'marginBottom';
+      break;
+    }
+    case 'column-reverse': {
+      after = 'marginTop';
+      before = 'marginBottom';
+      subAxis1 = 'marginLeft';
+      subAxis2 = 'marginRight';
+      break;
+    }
+  }
+
+  if (mainAxis === 'space-around') {
+    defaultStyle = { [after]: mSize, [subAxis1]: sSize, [subAxis2]: sSize };
+    specialStyle = { [after]: mSize, [before]: mSize, [subAxis1]: sSize, [subAxis2]: sSize };
+  } else {
+    defaultStyle = { [after]: mSize, [subAxis2]: sSize };
+    specialStyle = { [subAxis2]: sSize };
+  }
+
+  // 删除为 0 的值
+  defaultStyle = ObjectFilter(defaultStyle, (_, value) => value != 0);
+  specialStyle = ObjectFilter(specialStyle, (_, value) => value != 0);
+
+  return (
+    <div className={className} style={{ ...containerStyles, ...style }}>
+      {list.map((child, idx) => (
+        <div
+          key={idx}
+          style={{
+            ...(idx === specialPos ? specialStyle : defaultStyle),
+            ...itemStyle,
+          }}
+        >
+          {child}
+        </div>
+      ))}
+    </div>
+  );
+};
