e1217752a9cd7af9b727a61784a17e6173a958c4
OhYee <oyohyee@oyohyee.com>
2020-08-12 02:00:45 +0800

    fix: footer is too wide(#master)

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Flex } from '@/components/container';
+import Container, { Flex } from '@/components/container';
 import If from '@/components/if';
 import { RSS } from '@/components/svg';
 
@@ -12,38 +12,40 @@ export default function (props: {
 }) {
   const { beian, from, view, friends } = props;
   return (
-    <Flex direction="TB">
-      <a href="/rss.xml">
-        <RSS style={{ width: '1em' }} />
-        RSS订阅
-      </a>
-      <div>全站访问量 {view}</div>
-      <Flex direction="LR">
-        <div>
-          {from} – {new Date().getFullYear()}
-        </div>
-        <a href="http://beian.miit.gov.cn/">{beian}</a>
-      </Flex>
-      <div>
-        Powered by <a href="https://github.com/OhYee/blotter">Blotter</a>
-        (Go + React)
-      </div>
-      <Flex mainAxis="space-between">
-        <a href="/sitemap.txt">站点地图(TXT)</a>
-        <a href="/sitemap.xml">站点地图(XML)</a>
-      </Flex>
-      <If condition={!!friends && friends.length > 0}>
-        <Flex subSize="small">
-          {[
-            <strong>优秀博客订阅：</strong>,
-            ...friends.map((friend) => (
-              <a key={friend.name} href={friend.link}>
-                {friend.name}
-              </a>
-            )),
-          ]}
+    <Container>
+      <Flex direction="TB">
+        <a href="/rss.xml">
+          <RSS style={{ width: '1em' }} />
+          RSS订阅
+        </a>
+        <div>全站访问量 {view}</div>
+        <Flex direction="LR">
+          <div>
+            {from} – {new Date().getFullYear()}
+          </div>
+          <a href="http://beian.miit.gov.cn/">{beian}</a>
         </Flex>
-      </If>
-    </Flex>
+        <div>
+          Powered by <a href="https://github.com/OhYee/blotter">Blotter</a>
+          (Go + React)
+        </div>
+        <Flex mainAxis="space-between">
+          <a href="/sitemap.txt">站点地图(TXT)</a>
+          <a href="/sitemap.xml">站点地图(XML)</a>
+        </Flex>
+        <If condition={!!friends && friends.length > 0}>
+          <Flex mainAxis="center" subSize="small">
+            {[
+              <strong key="title">优秀博客订阅：</strong>,
+              ...friends.map((friend) => (
+                <a key={friend.name} href={friend.link}>
+                  {friend.name}
+                </a>
+              )),
+            ]}
+          </Flex>
+        </If>
+      </Flex>
+    </Container>
   );
 }
