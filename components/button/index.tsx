a40e286f3fb3a1501cd3e26f07f1b8e338790fb5
OhYee <oyohyee@oyohyee.com>
2020-08-20 18:05:08 +0800

    misc: button with shadow or neumorphism(#master)

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import { concat } from '@/utils/component';
+
 import styles from './button.less';
+import shadowStyles from '@/styles/shadow.less';
 
 export declare type ButtonProps = {
   size?: 'small' | 'middle' | 'large' | number;
@@ -8,17 +11,21 @@ export declare type ButtonProps = {
   icon?: React.ReactNode;
   circle?: boolean;
   primary?: boolean;
+  shadow?: boolean;
+  neumorphism?: boolean;
   className?: string;
   style?: React.CSSProperties;
   onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
 
-export default function Button(props: React.PropsWithChildren<ButtonProps>) {
+function Button(props: React.PropsWithChildren<ButtonProps>, ref) {
   const {
     size = 'middle',
     icon,
     circle = false,
     primary = false,
+    shadow = false,
+    neumorphism = false,
     onClick,
     style = {},
     className,
@@ -32,13 +39,18 @@ export default function Button(props: React.PropsWithChildren<ButtonProps>) {
     style.width = `${size + 1}em`;
     style.height = `${size + 1}em`;
   }
-  if (!!icon && !primary) classList.push(styles.icon);
+  if (!!icon) classList.push(styles.icon);
   if (circle) classList.push(styles.circle);
   if (primary) classList.push(styles.primary);
+  if (shadow) classList.push(shadowStyles.shadow, shadowStyles.clickable);
+  if (neumorphism) classList.push(shadowStyles.neumorphism, shadowStyles.clickable);
 
   return (
-    <button className={classList.filter((s) => s != '').join(' ')} style={style} onClick={onClick}>
+    <button className={concat(...classList)} style={style} onClick={onClick}>
       {!!icon ? icon : children}
     </button>
   );
 }
+
+// Next.js 的 Link  组件需要子组件可以接收 ref（用于修i该子元素的 href）
+export default React.forwardRef(Button);
