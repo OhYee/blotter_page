12e7de8f5d172f6ef50f0f11dcedf33653d87240
OhYee <oyohyee@oyohyee.com>
2020-08-21 20:58:40 +0800

    misc: button style a link component(#master)

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { concat } from '@/utils/component';
+import { concat, ComponentProps } from '@/utils/component';
 
 import styles from './button.less';
 import shadowStyles from '@/styles/shadow.less';
 
-export declare type ButtonProps = {
+export declare type ButtonProps = ComponentProps<{
   size?: 'small' | 'middle' | 'large' | number;
 
   icon?: React.ReactNode;
@@ -15,10 +15,10 @@ export declare type ButtonProps = {
   neumorphism?: boolean;
   className?: string;
   style?: React.CSSProperties;
-  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-};
+  onClick?: (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>) => void;
+}>;
 
-function Button(props: React.PropsWithChildren<ButtonProps>, ref) {
+function parseProps(props: ButtonProps) {
   const {
     size = 'middle',
     icon,
@@ -44,7 +44,11 @@ function Button(props: React.PropsWithChildren<ButtonProps>, ref) {
   if (primary) classList.push(styles.primary);
   if (shadow) classList.push(shadowStyles.shadow, shadowStyles.clickable);
   if (neumorphism) classList.push(shadowStyles.neumorphism, shadowStyles.clickable);
+  return { classList, style, onClick, icon, children };
+}
 
+function Button(props: ButtonProps, ref) {
+  const { classList, style, onClick, icon, children } = parseProps(props);
   return (
     <button className={concat(...classList)} style={style} onClick={onClick}>
       {!!icon ? icon : children}
@@ -52,5 +56,31 @@ function Button(props: React.PropsWithChildren<ButtonProps>, ref) {
   );
 }
 
+export declare type ALinkProps = ButtonProps & {
+  href?: string;
+  target?: string;
+  rel?: string;
+  linkType?: string;
+};
+function ALink(props: ALinkProps, ref: React.LegacyRef<HTMLAnchorElement>) {
+  const { classList, style, onClick, icon, children } = parseProps(props);
+  const { href, target, rel, linkType } = props;
+  return (
+    <a
+      ref={ref}
+      className={concat(...classList)}
+      style={style}
+      onClick={onClick}
+      href={href}
+      target={target}
+      rel={rel}
+      type={linkType}
+    >
+      {!!icon ? icon : children}
+    </a>
+  );
+}
+
 // Next.js 的 Link  组件需要子组件可以接收 ref（用于修i该子元素的 href）
 export default React.forwardRef(Button);
+export const A = React.forwardRef(ALink);
