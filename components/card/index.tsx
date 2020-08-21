a3dd6d207a49f1ac3e98d262ea06e88360a65533
OhYee <oyohyee@oyohyee.com>
2020-08-21 21:00:17 +0800

    misc: card component add inset neumorphism style(#master)

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -11,15 +11,25 @@ export default function Card(
   props: React.PropsWithChildren<{
     shadow?: boolean;
     neumorphism?: boolean;
+    neumorphismInset?: boolean;
     cover?: React.ReactNode;
     className?: string;
     style?: React.CSSProperties;
   }>,
 ) {
-  const { shadow = false, neumorphism = false, cover, className, style, children } = props;
+  const {
+    shadow = false,
+    neumorphism = false,
+    neumorphismInset = false,
+    cover,
+    className,
+    style,
+    children,
+  } = props;
   var classList = [styles.card, className];
   if (shadow) classList.push(shadowStyles.shadow);
   if (neumorphism) classList.push(shadowStyles.neumorphism);
+  if (neumorphismInset) classList.push(shadowStyles.neumorphism_inset);
 
   return (
     <div className={concat(...classList)} style={style}>
