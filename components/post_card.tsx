36bc75a6b3230bec8c9274aec8e93175b999a2c5
OhYee <oyohyee@oyohyee.com>
2020-08-21 21:01:48 +0800

    misc: Using CardContent component as Card component's content(#master)

--- components/post_card.tsx
+++ components/post_card.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Skeleton } from 'antd';
 import Link from 'next/link';
+import Router from 'next/router';
 
 import Image from '@/components/image';
 import TagPart from '@/components/tag';
 import Card from '@/components/card';
 import { Flex } from '@/components/container';
 import { Eye, Calendar, Tag, Edit } from '@/components/svg';
+import { A } from '@/components/button';
 
 import { concat } from '@/utils/component';
 
@@ -17,6 +19,47 @@ type PostCardProps = {
   post: Blotter.PostCard | undefined;
   loading: boolean;
 };
+
+export function CardContent(props: { post: Blotter.PostCard; editable?: boolean }) {
+  const { editable = false, post } = props;
+  return (
+    <Flex direction="TB" fullWidth>
+      <h2 className={concat(textStyles.color, textStyles.ellipsis)} title={post.title}>
+        {post.title}
+      </h2>
+      <p className={textStyles.color}>{post.abstract}</p>
+
+      <Flex mainAxis="flex-start">
+        <Flex mainAxis="flex-start" mainSize="small">
+          <Eye />
+          {post.view}
+        </Flex>
+        <Flex mainAxis="flex-start" mainSize="small">
+          <Calendar />
+          {post.publish_time}
+        </Flex>
+        {post.publish_time == post.edit_time ? null : (
+          <Flex mainAxis="flex-start" mainSize="small">
+            <Edit />
+            {post.edit_time}
+          </Flex>
+        )}
+        {editable ? (
+          <Link href={`/admin/post?url=${post.url}`} passHref>
+            <A primary neumorphism size="small" target="_blank">
+              编辑
+            </A>
+          </Link>
+        ) : null}
+      </Flex>
+
+      <Flex mainAxis="flex-start">
+        {[<Tag />, ...post.tags.map((tag: Blotter.Tag) => <TagPart key={tag.short} tag={tag} />)]}
+      </Flex>
+    </Flex>
+  );
+}
+
 export default function PostCard(props: PostCardProps) {
   const post = props.post as Blotter.PostCard;
   const loading = props.loading || typeof props.post === 'undefined';
@@ -37,40 +80,11 @@ export default function PostCard(props: PostCardProps) {
     >
       <Skeleton loading={loading} active>
         {loading ? null : (
-          <Flex direction="TB" fullWidth>
-            <Link href="/post/[url]" as={`/post/${post.url}`}>
-              <a>
-                <h2 className={concat(textStyles.color, textStyles.ellipsis)} title={post.title}>
-                  {post.title}
-                </h2>
-                <p className={textStyles.color}>{post.abstract}</p>
-              </a>
-            </Link>
-
-            <Flex mainAxis="flex-start">
-              <Flex mainAxis="flex-start" mainSize="small">
-                <Eye />
-                {post.view}
-              </Flex>
-              <Flex mainAxis="flex-start" mainSize="small">
-                <Calendar />
-                {post.publish_time}
-              </Flex>
-              {post.publish_time == post.edit_time ? null : (
-                <Flex mainAxis="flex-start" mainSize="small">
-                  <Edit />
-                  {post.edit_time}
-                </Flex>
-              )}
-            </Flex>
-
-            <Flex mainAxis="flex-start">
-              {[
-                <Tag />,
-                ...post.tags.map((tag: Blotter.Tag) => <TagPart key={tag.short} tag={tag} />),
-              ]}
-            </Flex>
-          </Flex>
+          <Link href="/post/[url]" as={`/post/${post.url}`}>
+            <a className={textStyles.color}>
+              <CardContent post={post} />
+            </a>
+          </Link>
         )}
       </Skeleton>
     </Card>
