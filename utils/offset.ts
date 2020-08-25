757bf0cd9b09935443b54d20ab7c073be34967bd
OhYee <oyohyee@oyohyee.com>
2020-08-25 22:25:56 +0800

    misc: add getOffsetLeft function(#master)

--- utils/offset.ts
+++ utils/offset.ts
@@ -1,5 +1,9 @@
-function getOffsetTop(e: HTMLElement): number {
+export function getOffsetTop(e: HTMLElement): number {
   return e.offsetTop + (e.offsetParent ? getOffsetTop(e.offsetParent as any) : 0);
+}
+
+export function getOffsetLeft(e: HTMLElement): number {
+  return e.offsetLeft + (e.offsetParent ? getOffsetLeft(e.offsetParent as any) : 0);
 }
 
 export default getOffsetTop;
