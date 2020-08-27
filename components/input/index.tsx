a0167f2e197af7cf4170a5ef4b761a7cbcfb9b19
OhYee <oyohyee@oyohyee.com>
2020-08-27 17:30:54 +0800

    feat: new version input and checkbox component(#master)

---
+++ components/input/index.tsx
@@ -0,0 +1,6 @@
+import Input, { InputProps, Option } from './input';
+import CheckBox, { CheckBoxProps } from './checkbox';
+
+export default Input;
+export { CheckBox };
+export type { InputProps, CheckBoxProps, Option };
