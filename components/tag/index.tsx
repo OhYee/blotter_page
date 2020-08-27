58e269057ce3ed61e925db9995570f730186115b
OhYee <oyohyee@oyohyee.com>
2020-08-28 01:04:29 +0800

    misc: auto generate front color by background color for tag(#master)

--- components/tag/index.tsx
+++ components/tag/index.tsx
@@ -7,11 +7,47 @@ import Avatar from '@/components/avatar';
 import { concat, ComponentProps } from '@/utils/component';
 import { Flex } from '@/components/container';
 
-import { reverseColor, isLight } from '@/utils/color';
+import { reverseColor, isLight, parseColor, toHex, rgb2hsl, hsl2rgb } from '@/utils/color';
 
 import styles from './tag.less';
 import textStyles from '@/styles/text.less';
 import shadowStyles from '@/styles/shadow.less';
+
+// function getFrontColor(backgroundColor) {
+//   /*
+//         根据背景色计算前景色
+
+//         首先根据解析出三原色，判断是浅色还是深色，而后将这些颜色朝另一个方向按比例移动
+
+//         浅色背景 xx = x - 3 * fixValue * x / (r + g + b)
+//                 xx = (1 - 3 * fixValue / (r + g + b)) * x
+//         深色背景 xx = x + 3 * fixValue * (255 - x) / (255 * 3 - (r + g + b))
+//                 xx = (1 - 3 * fixValue / (255 * 3 - (r + g + b)) * x + 3 * fixValue * 255 / (255 * 3 - (r + g + b))
+//     */
+//   const [r, g, b] = parseColor(backgroundColor);
+//   const total = r + g + b;
+//   const fixValue = (Math.pow(127 - Math.min(total / 3, 255 - total / 3) / 127, 4) + 1) * 127;
+
+//   // y = ax + b
+//   const _a = total / 3 > 127 ? 1 - (3 * fixValue) / total : 1 - (3 * fixValue) / (255 * 3 - total);
+//   const _b = total / 3 > 127 ? 0 : (3 * fixValue * 255) / (255 * 3 - total);
+
+//   const rr = _a * r + _b;
+//   const gg = _a * g + _b;
+//   const bb = _a * b + _b;
+
+//   return `#${toHex(rr)}${toHex(gg)}${toHex(bb)}`;
+// }
+
+function getFrontColor(backgroundColor) {
+  const [r, g, b] = parseColor(backgroundColor);
+  const [h, s, l] = rgb2hsl(r, g, b);
+  const hh = h;
+  const ss = 100;
+  const ll = l > 50 ? l - 50 : l <= 25 ? 100 - l : s > 50 ? 100 : l + 50;
+  const [rr, gg, bb] = hsl2rgb(hh, ss, ll);
+  return `#${toHex(rr)}${toHex(gg)}${toHex(bb)}`;
+}
 
 type TagProps = ComponentProps<{
   tag?: Blotter.Tag;
@@ -38,7 +74,7 @@ export default function Tag(props: TagProps) {
             ...(!!tag.color
               ? {
                   background: tag.color,
-                  color: isLight(tag.color) ? 'var(--color-dark)' : 'var(--color-white)',
+                  color: getFrontColor(tag.color),
                 }
               : {}),
             ...style,
--- utils/color.ts
+++ utils/color.ts
@@ -148,7 +148,9 @@ export const RGBHEX = /(^#([0-9A-F]{1,2})([0-9A-F]{1,2})([0-9A-F]{1,2})$)/i;
 export const RGB = /^rgb\((\d+),(\d+),(\d+)\)$/i;
 
 export function toHex(n: number) {
-  return n.toString(16).padStart(2, '0');
+  return Math.min(255, Math.max(0, Math.round(n)))
+    .toString(16)
+    .padStart(2, '0');
 }
 
 export function parseColor(c: string) {
@@ -186,4 +188,80 @@ export function isLight(c: string) {
   const [r, g, b] = parseColor(c);
   const l = (Math.max(r, g, b) + Math.min(r, g, b)) / 2;
   return l > 255 / 2;
+}
+
+export function rgb2hsl(r: number, g: number, b: number) {
+  r = r / 255;
+  g = g / 255;
+  b = b / 255;
+
+  var min = Math.min(r, g, b);
+  var max = Math.max(r, g, b);
+  var difference = max - min;
+  var h,
+    s,
+    l = (min + max) / 2;
+  if (max == min) {
+    h = 0;
+    s = 0;
+  } else {
+    s = l > 0.5 ? difference / (2.0 - max - min) : difference / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / difference + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = 2.0 + (b - r) / difference;
+        break;
+      case b:
+        h = 4.0 + (r - g) / difference;
+        break;
+    }
+    h = Math.round(h * 60);
+  }
+  s = Math.round(s * 100); //转换成百分比的形式
+  l = Math.round(l * 100);
+  return [h, s, l];
+}
+
+export function hsl2rgb(h: number, s: number, l: number) {
+  var h = h / 360;
+  var s = s / 100;
+  var l = l / 100;
+  var rgb = [];
+
+  if (s == 0) {
+    rgb = [Math.round(l * 255), Math.round(l * 255), Math.round(l * 255)];
+  } else {
+    var q = l >= 0.5 ? l + s - l * s : l * (1 + s);
+    var p = 2 * l - q;
+    var tr = (rgb[0] = h + 1 / 3);
+    var tg = (rgb[1] = h);
+    var tb = (rgb[2] = h - 1 / 3);
+    for (var i = 0; i < rgb.length; i++) {
+      var tc = rgb[i];
+      if (tc < 0) {
+        tc = tc + 1;
+      } else if (tc > 1) {
+        tc = tc - 1;
+      }
+      switch (true) {
+        case tc < 1 / 6:
+          tc = p + (q - p) * 6 * tc;
+          break;
+        case 1 / 6 <= tc && tc < 0.5:
+          tc = q;
+          break;
+        case 0.5 <= tc && tc < 2 / 3:
+          tc = p + (q - p) * (4 - 6 * tc);
+          break;
+        default:
+          tc = p;
+          break;
+      }
+      rgb[i] = Math.round(tc * 255);
+    }
+  }
+
+  return rgb;
 }
