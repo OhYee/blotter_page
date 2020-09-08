542c95633fa7fefa239fbcb05f802c5aa18b7b15
OhYee <oyohyee@oyohyee.com>
2020-09-08 22:24:48 +0800

    feat: drawer component(#master)

---
+++ components/drawer/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { concat, ComponentProps } from '@/utils/component';
+
+import styles from './drawer.less';
+
+export declare type DrawerProps = ComponentProps<{
+  //   getContainer: () => HTMLElement;
+  placement?: 'left' | 'right' | 'top' | 'bottom';
+  show: boolean;
+  onClose?: () => void;
+  containerStyle?: React.CSSProperties;
+}>;
+
+export default function Drawer(props: DrawerProps) {
+  const {
+    // getContainer = () => document,
+    placement = 'left',
+    show,
+    onClose = () => {},
+    children,
+    className,
+    style,
+    id,
+    containerStyle,
+  } = props;
+  return (
+    <div className={concat(styles.drawer, show ? styles.show : '')} style={containerStyle}>
+      <div className={styles.dimmend} onClick={onClose}></div>
+      <div id={id} className={concat(styles.inner, styles[placement], className)} style={style}>
+        {children}
+      </div>
+    </div>
+  );
+}
