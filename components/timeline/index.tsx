9e151cfa39a4dcb069ade3da46e508e73d001200
OhYee <oyohyee@oyohyee.com>
2020-09-08 22:25:37 +0800

    feat: new timeline component(#master)

---
+++ components/timeline/index.tsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+
+import { Flex } from '@/components/container';
+
+import { concat, ComponentProps } from '@/utils/component';
+
+import styles from './timeline.less';
+
+export default function Timeline(
+  props: ComponentProps<{
+    direction?: 'LR' | 'TB';
+    data: { time: React.ReactNode; name: React.ReactNode }[];
+  }>,
+) {
+  const { direction = 'TB', data, className, style, id } = props;
+  const subDirection = direction == 'TB' ? 'LR' : 'TB';
+  return (
+    <div id={id} className={concat(styles.timeline, className)} style={style}>
+      {data.map((item) => (
+        // <div >
+        <React.Fragment key={item.time.toString()}>
+          <div className={styles.time}>{item.time}</div>
+          <div className={styles.circle}></div>
+          <div className={styles.line}></div>
+          <div className={styles.name}>{item.name}</div>
+        </React.Fragment>
+        // </div>
+      ))}
+    </div>
+  );
+}
