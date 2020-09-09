961570214e3d6c94c0ed350162c2de3b66d9f15d
OhYee <oyohyee@oyohyee.com>
2020-09-09 22:37:03 +0800

    misc: rewrite admin index page(#master)

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,10 +2,24 @@ import React, { ComponentProps } from 'react';
 
 import Head from 'next/head';
 
-import { Card, List, Typography } from 'antd';
+import Card from '@/components/card';
 
 import { Context } from '@/utils/global';
 import Link from 'next/link';
+
+import styles from './index.less';
+
+const data = [
+  { title: '标签管理', url: '/admin/tags' },
+  { title: '文章管理', url: '/admin/posts' },
+  { title: '订阅管理', url: '/admin/friends' },
+  { title: '菜单管理', url: '/admin/menus' },
+  { title: '变量管理', url: '/admin/variables' },
+  { title: '评论管理', url: '/admin/comments' },
+  { title: '用户管理', url: '/admin/users' },
+  { title: '游记管理', url: '/admin/travels' },
+  { title: '上传管理', url: '/admin/upload' },
+];
 
 interface AdminIndexProps extends ComponentProps<'base'> {}
 
@@ -23,7 +37,7 @@ class AdminIndex extends React.Component<AdminIndexProps, AdminIndexState> {
 
   render() {
     return (
-      <Card>
+      <Card neumorphism>
         <Context.Consumer>
           {(context) => (
             <Head>
@@ -31,28 +45,15 @@ class AdminIndex extends React.Component<AdminIndexProps, AdminIndexState> {
             </Head>
           )}
         </Context.Consumer>
-        <List
-          dataSource={[
-            { title: '标签管理', url: '/admin/tags' },
-            { title: '文章管理', url: '/admin/posts' },
-            { title: '订阅管理', url: '/admin/friends' },
-            { title: '菜单管理', url: '/admin/menus' },
-            { title: '变量管理', url: '/admin/variables' },
-            { title: '评论管理', url: '/admin/comments' },
-            { title: '用户管理', url: '/admin/users' },
-            { title: '游记管理', url: '/admin/travels' },
-            { title: '上传管理', url: '/admin/upload' },
-          ]}
-          renderItem={(item) => (
-            <List.Item key={item.title}>
-              <Typography.Text>
-                <Link href={item.url}>
-                  <a>{item.title}</a>
-                </Link>
-              </Typography.Text>
-            </List.Item>
-          )}
-        />
+        <ul className={styles.list}>
+          {data.map((item) => (
+            <li key={item.title}>
+              <Link href={item.url}>
+                <a>{item.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </Card>
     );
   }
