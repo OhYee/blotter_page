a2eeac1c65c147afaa997a2c37226162cac319b9
OhYee <oyohyee@oyohyee.com>
2020-09-11 18:24:29 +0800

    feat: TeatArea component transform support(#master)

--- components/input/textarea/index.tsx
+++ components/input/textarea/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import { Edit } from '@/components/svg';
+
 import { concat, ComponentProps } from '@/utils/component';
+
+import { BasePartProps, InputPartProps, TransformPartProps } from '../input';
 
 import shadowStyles from '@/styles/shadow.less';
 import styles from './textarea.less';
@@ -10,28 +14,13 @@ export declare type Option<T> = {
   value?: T;
 };
 
-export declare type TextAreaProps = ComponentProps<{
-  label?: string;
-  lablePlacement?: 'left' | 'top';
-  size?: 'small' | 'middle' | 'large';
-  prefix?: React.ReactNode;
-  suffix?: React.ReactNode;
-  disabled?: boolean;
-  onEnterPressed?: () => void;
-  onBlur?: () => void;
-  hint?: React.ReactNode;
-
-  // textarea
-  value?: string;
-  defaultValue?: string;
-  editable?: boolean;
-  placeholder?: string;
-  onChange?: (value: string) => void;
-  getValueCallback?: (callback: () => string) => void;
-  setValueCallback?: (callback: (value: string) => void) => void;
-  autoFocus?: boolean;
-  rows?: number;
-}>;
+export declare type TextAreaProps = ComponentProps<
+  {
+    rows?: number;
+  } & BasePartProps &
+    InputPartProps &
+    TransformPartProps
+>;
 
 export default function TextArea(props: TextAreaProps) {
   const {
@@ -53,6 +42,7 @@ export default function TextArea(props: TextAreaProps) {
     hint,
     style,
     className,
+    transform = false,
   } = props;
   const ref = React.useRef<HTMLTextAreaElement>();
   React.useEffect(() => getValueCallback(() => ref.current.value), [ref, getValueCallback]);
@@ -61,28 +51,46 @@ export default function TextArea(props: TextAreaProps) {
     setValueCallback,
   ]);
 
+  const [showInput, setShowInput] = React.useState(false);
   return (
     <div
       className={concat(styles.wrapper, className, styles[size], styles[lablePlacement])}
       style={style}
-      onBlur={onBlur}
+      onBlur={() => {
+        if (transform) setShowInput(false);
+        if (!!onBlur) onBlur();
+      }}
+      onClick={() => {
+        if (transform) setShowInput(true);
+      }}
     >
-      {!!label ? <div className={styles.label}>{label}</div> : null}
-      <div className={concat(styles.inner, ...(disabled ? ['disabled'] : []))}>
-        <div className={concat(styles.textarea, shadowStyles.neumorphism_inset)}>
-          <textarea
-            ref={ref}
-            defaultValue={!!value ? undefined : defaultValue}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder={placeholder}
-            readOnly={!editable}
-            autoFocus={autoFocus}
-            rows={rows}
-          />
+      {!transform || (transform && showInput) ? (
+        <React.Fragment>
+          {!!label ? <div className={styles.label}>{label}</div> : null}
+          <div className={concat(styles.inner, ...(disabled ? ['disabled'] : []))}>
+            <div className={concat(styles.textarea, shadowStyles.neumorphism_inset)}>
+              <textarea
+                ref={ref}
+                defaultValue={!!value ? undefined : defaultValue}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+                placeholder={placeholder}
+                readOnly={!editable}
+                autoFocus={autoFocus}
+                rows={rows}
+              />
+            </div>
+            {!!hint ? <div className={styles.hint}>{hint}</div> : null}
+          </div>
+        </React.Fragment>
+      ) : (
+        <div className={styles.transform}>
+          <span className={styles.transform_value}>{value}</span>
+          <span className={styles.transform_icon}>
+            <Edit />
+          </span>
         </div>
-        {!!hint ? <div className={styles.hint}>{hint}</div> : null}
-      </div>
+      )}
     </div>
   );
 }
