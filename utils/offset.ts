5d8899bc98083d1665ef7f9b7d3f12e4f7ed069c
OhYee <oyohyee@oyohyee.com>
2020-09-12 11:41:54 +0800

    misc: get width for inline(#master)

--- utils/offset.ts
+++ utils/offset.ts
@@ -6,4 +6,15 @@ export function getOffsetLeft(e: HTMLElement): number {
   return e.offsetLeft + (e.offsetParent ? getOffsetLeft(e.offsetParent as any) : 0);
 }
 
+export function getInlineWidth(e: HTMLElement): number {
+  if (e.offsetWidth != 0) return e.offsetWidth;
+  const children = e.children;
+  var sum = 0;
+  for (var i = 0; i < children.length; i++) {
+    const child = children[i] as HTMLElement;
+    sum += getInlineWidth(child);
+  }
+  return sum;
+}
+
 export default getOffsetTop;
