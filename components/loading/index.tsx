de92af3f458fb6bde856f84233d06aa108781d8e
OhYee <oyohyee@oyohyee.com>
2020-09-14 23:39:24 +0800

    refactor: rewrite loading component. render with children(#master)

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,13 +1,22 @@
+import { concat, ComponentProps } from '@/utils/component';
+
 import styles from './loading.less';
-export default function Loading(props: { count?: number }) {
-  const { count = 5 } = props;
+
+export default function Loading(props: ComponentProps<{ count?: number; loading?: boolean }>) {
+  const { count = 5, loading = true, className, children, ...restProps } = props;
   return (
-    <div className={styles.loading}>
-      {Array(count)
-        .fill(0)
-        .map((_, idx) => (
-          <span key={idx} style={{ animationDelay: `${idx * 0.2}s` }}></span>
-        ))}
+    <div
+      className={concat(styles.wrapper, loading ? styles.onloading : '', className)}
+      {...restProps}
+    >
+      <div className={styles.loading}>
+        {Array(count)
+          .fill(0)
+          .map((_, idx) => (
+            <span key={idx} style={{ animationDelay: `${idx * 0.2}s` }}></span>
+          ))}
+      </div>
+      {children && <div className={styles.inner}>{children}</div>}
     </div>
   );
 }
