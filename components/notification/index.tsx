d8085c999ef0821ad902c8bfd8641dd73253db54
OhYee <oyohyee@oyohyee.com>
2020-09-27 21:43:09 +0800

    misc: set notification list style z index(#master)

--- components/notification/index.tsx
+++ components/notification/index.tsx
@@ -104,7 +104,7 @@ function Messages(props: { messages: MessagePropsWithID[] }) {
   return (
     <Flex
       direction="TB"
-      style={{ position: 'fixed', top: 20, right: 20, width: '300px' }}
+      style={{ position: 'fixed', top: 20, right: 20, width: '300px', zIndex: 2 }}
       itemStyle={{ width: '100%' }}
     >
       {messages.map((p, i) => {
