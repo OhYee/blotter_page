4c69fc02a18e9c0ac711491e95d24bee67156e42
OhYee <oyohyee@oyohyee.com>
2020-10-17 11:20:17 +0800

    misc: add spacing before friends and reduce font size for footer(#master)

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,6 +4,8 @@ import { Flex } from '@/components/container';
 import If from '@/components/if';
 import { RSS } from '@/components/svg';
 
+import styles from './footer.module.scss';
+
 export default function Footer(props: {
   beian: string;
   from: string;
@@ -12,7 +14,7 @@ export default function Footer(props: {
 }) {
   const { beian, from, view, friends } = props;
   return (
-    <Flex direction="TB">
+    <Flex direction="TB" className={styles.footer}>
       <Flex wrap={false}>
         <RSS style={{ color: '#EE802F' }} />
         <a href="/rss.xml">RSS订阅</a>
@@ -33,7 +35,7 @@ export default function Footer(props: {
         <a href="/sitemap.xml">站点地图(XML)</a>
       </Flex>
       <If condition={!!friends && friends.length > 0}>
-        <Flex mainAxis="center" subSize="small">
+        <Flex mainAxis="center" subSize="small" className={styles.friends}>
           {[
             <strong key="title">优秀博客订阅：</strong>,
             ...friends.map((friend) => (
