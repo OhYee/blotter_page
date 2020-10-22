c9e0967b6fd40cdacf1067b695ccab1fa15b1b8b
OhYee <oyohyee@oyohyee.com>
2020-10-22 17:27:00 +0800

    fix: set fullscreen mode below image lightbox(#master)

--- pages/admin/post/index.tsx
+++ pages/admin/post/index.tsx
@@ -447,7 +447,7 @@ class PostEdit extends React.Component<PostEditProps, PostEditState> {
               this.editor.layout();
             });
           }}
-          style={this.state.fullscreen ? { zIndex: 99999 } : {}}
+          style={this.state.fullscreen ? { zIndex: 998 } : {}}
         />
 
         <Flex direction="TB" fullWidth>
@@ -467,7 +467,7 @@ class PostEdit extends React.Component<PostEditProps, PostEditState> {
                     right: 0,
                     background: 'var(--background)',
                     width: '100%',
-                    zIndex: 99998,
+                    zIndex: 997,
                   }
                 : {}
             }
