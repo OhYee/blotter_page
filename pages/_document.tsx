ab7bf5b54785dc64a29ad1502ecaf4a05246ca0f
OhYee <oyohyee@oyohyee.com>
2020-10-24 16:23:06 +0800

    misc: move baidu js to head variable(#master)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,37 +27,6 @@ export default class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#1565c0" />
           <meta name="msapplication-TileImage" content="/static/img/logo_196x196.png" />
           <link rel="shortcut icon" href="/static/img/favicon.ico"></link>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-window.onload = function() { 
-    // 百度自动推送
-    (function(){
-        var bp = document.createElement('script');
-        var curProtocol = window.location.protocol.split(':')[0];
-        if (curProtocol === 'https') {
-            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-        }
-        else {
-            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-        }
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(bp, s);
-    })();
-
-    // 百度统计
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?9f114bc8c6227268e6d0e44abd656932";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-    })();
-
-}            
-`,
-            }}
-          ></script>
         </Head>
         <body>
           <Main />
