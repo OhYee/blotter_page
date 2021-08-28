eab3a5481211efde6dd3033df73426506f019c6a
OhYee <oyohyee@oyohyee.com>
2021-08-29 01:46:06 +0800

    feat: Docker 化运行

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,23 @@
-FROM node:15.14.0
+FROM node:16.5.0 AS builder
 
-ENV backendURI="http://backend:50000"
-
-COPY . /data/blotter_page
+COPY ./ /data/blotter_page
 
 WORKDIR /data/blotter_page
 
-ENTRYPOINT [ "yarn", "start"]
+RUN yarn
+RUN yarn build
+
+FROM node:16.5.0 AS prod
+
+ENV backendURI="http://backend:50000"
+
+COPY --from=builder /data/blotter_page/.next /data/blotter_page/.next
+# COPY --from=builder /data/blotter_page/node_modules /data/blotter_page/node_modules
+COPY --from=builder /data/blotter_page/package.json /data/blotter_page/package.json
+COPY --from=builder /data/blotter_page/yarn.lock /data/blotter_page/yarn.lock
+COPY --from=builder /data/blotter_page/next.config.js /data/blotter_page/next.config.js
+
+WORKDIR /data/blotter_page
+RUN yarn --prod
+
+ENTRYPOINT [ "yarn", "start" ]
--- build.bash
+++ build.bash
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-IMAGE="node:15.14.0"
-
-
-# 拉取镜像
-if [[ $(docker images ${IMAGE} | wc -l) -eq "1" ]]; then
-    echo "Pulling docker image ${IMAGE}..."
-    docker pull ${IMAGE}
-fi
-
-# 更新代码
-echo "Pulling latest code..."
-git pull
-
-
-# 在 docker 中挂载当前目录编译
-echo "Building execute in docker..."
-
-docker run --rm \
-    -v ${SHELL_FOLDER}:/data/blotter_page \
-    ${IMAGE} \
-    bash -c "cd /data/blotter_page && echo 'Generating...' && yarn && yarn build"
-
-echo "Build finished"
-
-docker build -t blotter_page .
-
-echo "Docker image 'blotter' build finished."
-echo 'Using `docker run --rm --name=frontend --link backend:backend blotter_page` to start server.'
+TAG=$(git describe --abbrev=0 --tags 2>/dev/null || git rev-parse --short HEAD)
+IMAGE="ohyee/blotter_page:${TAG}"
+echo $IMAGE
+docker build -t ${IMAGE} .
